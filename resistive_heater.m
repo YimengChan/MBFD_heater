cb70fa19aafa32b72b49b600194345cd2ace75e7
YimengChan <32922683+YimengChan@users.noreply.github.com>
2017-11-03 18:47:44 -0700

    -m 'added material properties'

--- resistive_heater.m
+++ resistive_heater.m
@@ -23,9 +23,38 @@ homogeneous_temp_fluid_side = 323; %[K]
 T_heater_initial = ones(n_segments,1).*homogeneous_temp_heater_side;
 T_fluid_initial = ones(n_segments,1).*homogeneous_temp_fluid_side;
 
+%input heater power profile. 
+%Assume that heater power is a constant 
+p_total = 10e3; %[W]
+p_profile = ones(n_segments,1).*p_total/n_segments;
+
+%Temp dependent properties of dowtherm A. T in Kelvin
+density_oil = @(T) 1078-(0.85*(T-273)); %[kg/m^3]
+viscosity_oil = @(T) 0.130/((T-273)^1.072); %[kg/m s]
+k_oil = @(T) 0.142 - (0.00016*(T-273)); %[W/m C]
+c_p_oil = @(T) 1518 + 2.82.*(T-273); %[J/kg C]
+
+%Temp dependent properties of steel. T in Kelvin
+k_steel = @(T) 14.6 + (0.0127*(T-273));
+
+%Temp dependent properties of insulation. T in Kelvin
+k_insulation = @(T) 0.206 + (7.702e-4)*(T-273);
+
+
+%Nusselt correlation for turbulent fluids
+Nu = @(Re,Pr) 0.024*(Re^0.8)*(Pr^0.33);
+
+%Pr definition
+Re 
+
+
 %create storer
 T_heater{1} = T_heater_initial;
 T_fluid{1} = T_fluid_initial;
+
+
+
+
 
 %heat equation for resister heater
 %rate of change = power added - heat loss to coolant + heat diffusion due
@@ -41,6 +70,9 @@ for t = 1:t_segments
         %special consideration for the first 2 and last 2 values
         d2T_dx2(i-2) = (-d2T_data(i+2) + 16*d2T_data(i+1)-30*d2T_data(i)+16*d2T_data(i-1)-d2T_data(i-2))/(12*(x_step^2));
     end
+    
+    %Fluid equation: c_p(T(i))*x_step*pi*r^2*density*T(i)' = m_flow.*(T(i) -
+    %T(i-1)
     
     %update Temperature profile at time t
     
